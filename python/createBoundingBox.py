63df3c237bbd81f4a461db1eab6dd2caa8425288
Marrit <marrit.leenstra@wur.nl>
2019-01-23 17:00:27 +0100

    deleted geopy dependency in main script

--- python/createBoundingBox.py
+++ python/createBoundingBox.py
@@ -3,11 +3,13 @@
 # Authors: Anne-Juul Welsink and Marrit Leenstra
 # Date: 22-01-2019
 
-def createBoundingBox(x, y):
+def createBoundingBox(x, y, width, height):
     """create bounding box around point location
     
     :x: x-coordinate
     :y: y-coordinate
+    :width: width of the bounding box
+    :height: height of the bounding box
     return: bounding box (xmin, ymin, xmax, ymax)
     """
-    return (x-500, y-500, x+500, y+500)
+    return (x-(width/2), y-(height/2), x+(width/2), y+(height/2))
